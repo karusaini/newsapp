48bcc61889683b5f1b5d4fcbde52f46ca1091547
karusaini <karinasaini06@gmail.com>
2023-05-08 20:49:30 +0530

    NewsApp

---
+++ src/components/NewsItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://cdn.ndtv.com/common/images/ogndtv.png"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+              <span className="badge bg-secondary">New</span>
+            </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on
+                {new Date(date).toGMTString()} 3 mins ago
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
